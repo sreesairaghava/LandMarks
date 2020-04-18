909c97fe98d09768a5ce7c40a8a0008b33c8cb2d
Sree Sai Raghava <raghava.rym@gmail.com>
2020-04-18 16:57:06 +0530

    Initial Commit

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Sree Sai Raghava Dandu on 18/04/20.
+//  Copyright © 2020 Sree Sai Raghava. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
